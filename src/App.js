dad1ff218b45ae0c99d32386554953b0a4686fb0
Matthew Young <matthew.young@ramseysolutions.com>
2022-11-08 11:21:50 -0600

    refactor to accomodate new patterns

--- src/App.js
+++ src/App.js
@@ -1,37 +1,12 @@
 import "./App.css";
-import { Observable, generateData, logger } from "./observable";
-import Button from "@ramsey-design-system/button";
-import "@ramsey-design-system/button/dist/button.css";
-import Stack from "@ramsey-design-system/stack";
-import "@ramsey-design-system/stack/dist/stack.css";
+import { ObservablePattern } from "./patterns/obersvable-pattern/observablePattern";
 
 function App() {
-  const observedObject = new Observable();
-
-  const handleUpdate = () => {
-    observedObject.notify(generateData());
-  };
-
-  const handleSubscribe = () => {
-    if (!observedObject.observers.includes(logger)) {
-      observedObject.subscribe(logger);
-      console.log("Subscribed");
-    } else {
-      observedObject.unsubscribe(logger);
-      console.log("Unsubscribed");
-    }
-  };
-
   return (
     <div className="App">
-      <header className="App-header">
-        <Stack>
-          <Button onClick={handleSubscribe}>
-            Subscribe/Unsubscribe Logger
-          </Button>
-          <Button onClick={handleUpdate}>Update Weather Data</Button>
-        </Stack>
-      </header>
+      <body>
+        <ObservablePattern />
+      </body>
     </div>
   );
 }
